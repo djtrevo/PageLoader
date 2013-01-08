14b870c0474ed3c048310f0318fcc8ed82d2cb76
Marco Trevisani <info@djtrevo.com>
2013-01-08 18:18:47 +0100

    First Release

---
+++ include/jquery.trevo.page.loader.js
@@ -0,0 +1,62 @@
+/*!
+ * jQuery TrEVo Page Loader v1.0
+ * https://github.com/djtrevo/HeightMaximizer
+ *
+ * Copyright 2012, Marco Trevisani
+ * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Official Website: http://www.marcotrevisani.com
+ * Music Website: http://www.djtrevo.com
+ *
+ * * * * * * * * * * * * * * * * * * * * * *
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.opensource.org/licenses/GPL-2.0
+ */ (function ($) {
+
+    var defaults = {
+        cssID: "loading",
+        fadeOut: 1000,
+        fadeIn: 500,
+        onLoad: true
+    };
+
+    var methods = {
+        init: function (options) {
+
+            var options = $.extend(defaults, options);
+
+            return this.each(function () {
+
+                $("body").append("<div id=\"" + options.cssID + "\"></div>")
+
+                if (options.onLoad) {
+                    $(this).load(function () {
+                        jQuery("#" + options.cssID + "").fadeOut(options.fadeOut);
+                    });
+                }
+            });
+
+        },
+        show: function () {
+            var options = $.extend(defaults, options);
+            jQuery("#" + options.cssID + "").fadeIn(options.fadeIn);
+
+        },
+        hide: function () {
+            var options = $.extend(defaults, options);
+            jQuery("#" + options.cssID + "").fadeOut(options.fadeOut);
+        },
+    };
+
+    $.fn.pageLoader = function (methodOrOptions) {
+        if (methods[methodOrOptions]) {
+            return methods[methodOrOptions].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof methodOrOptions === 'object' || !methodOrOptions) {
+            // Default to "init"
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.pageLoader');
+        }
+    };
+})(jQuery);
